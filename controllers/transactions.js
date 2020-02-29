992fbee9240b95e8baa3ef9c1a8aeaf71a8ad7da
Andres Gutierrez <agutierrez.er@gmail.com>
2020-02-29 15:18:14 +0100

    Add Add, delete controllers

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,6 +1,20 @@
-// @desc    Get all transctions
+// @desc    Get all Transactions
 // @route   GET /api/v1/transactions
 // @access  Public
 exports.getTransactions = (req, res, next) => {
-  res.send('GET Transactions');
+  res.send('GET Transactions')
+}
+
+// @desc    Add Transactions
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransaction = (req, res, next) => {
+  res.send('POST Transaction')
+}
+
+// @desc    Delete Transactions
+// @route   DELETE /api/v1/transactions/:id
+// @access  Public
+exports.deleteTransaction = (req, res, next) => {
+  res.send('DELETE Transaction')
 }
