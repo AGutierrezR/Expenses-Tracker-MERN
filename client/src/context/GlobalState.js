d5e9634446b477e7f7ff8d157fd6636e0362c7b0
Andres Gutierrez <agutierrez.er@gmail.com>
2020-02-29 23:22:10 +0100

    Add getTransactions to GlobalState
    It will fetch data from the api using axios

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from 'react'
 import AppReducer from './AppReducer'
+import axios from 'axios'
 
 // Initial state
 const initialState = {
@@ -12,6 +13,23 @@ export const GlobalContext = createContext(initialState)
 // Provider Component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState)
+
+  // Actions
+  async function getTransactions() {
+    try {
+      const res = await axios.get('api/v1/transactions')
+
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data
+      })
+    } catch (error) {
+      dispatch({
+        type: 'TRANSACTIONS_ERROR',
+        payload: error.response.data.error
+      })
+    }
+  }
 
   function deleteTransaction(id) {
     dispatch({
@@ -32,6 +50,7 @@ export const GlobalProvider = ({ children }) => {
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
+        getTransactions,
         deleteTransaction,
         addTransaction
       }}>
