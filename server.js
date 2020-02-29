870cc0e4153621efb9d8ea621bb6f5661766541b
Andres Gutierrez <agutierrez.er@gmail.com>
2020-02-29 14:45:59 +0100

    Create routes features

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const morgan = require('morgan')
 
 dotenv.config({ path: './config/config.env' })
 
+const transactions = require('./routes/transactions')
+
 const app = express()
 
-app.get('/', (req, res) => res.send('hello'))
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+  )
+)
