0a42400be23279c833c742812037bb5b3ecf3913
Andres Gutierrez <agutierrez.er@gmail.com>
2020-02-29 15:38:32 +0100

    Add transaction controller for GET
    Every request to the server has to use async
    
    In GET we use await to wait for the transactions and them pass the response has json

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction')
+
 // @desc    Get all Transactions
 // @route   GET /api/v1/transactions
 // @access  Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET Transactions')
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find()
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    })
+  } catch (error) {
+    return res.send(500).json({
+      success: false,
+      error: 'Server Error'
+    })
+  }
 }
 
 // @desc    Add Transactions
 // @route   POST /api/v1/transactions
 // @access  Public
-exports.addTransaction = (req, res, next) => {
-  res.send('POST Transaction')
+exports.addTransaction = async (req, res, next) => {
+  res.send('Add Transaction')
 }
 
 // @desc    Delete Transactions
 // @route   DELETE /api/v1/transactions/:id
 // @access  Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE Transaction')
 }
